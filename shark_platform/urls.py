152c336161d7be6d14197d35218159a1ebeb81fe
newsmallsprout <847784217@qq.com>
2026-01-26 11:11:08 +0800

    a

--- shark_platform/urls.py
+++ shark_platform/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.contrib.auth import views as auth_views
-from core import views as core_views
+# from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
