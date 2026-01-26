baef65516128dde0733170ba21558fd047cde4d7
newsmallsprout <847784217@qq.com>
2026-01-26 11:43:31 +0800

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router', 'pinia'],
+          'element-plus': ['element-plus', '@element-plus/icons-vue'],
+          'echarts': ['echarts', 'vue-echarts'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
   },
   server: {
     proxy: {
